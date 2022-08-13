dd1388613f876471add6b80d03b60c9fcb9cdaa9
kirstyabhus <kabhulimhen@gmail.com>
2022-08-13 12:55:52 +0100

    Output the response & recipe

--- recipie search main.py
+++ recipie search main.py
@@ -1,13 +1,24 @@
 import requests
 
-ingredient = input("What ingredient do you want to search for? ")
-app_key = 'a28413f876f1d5d0b52d989a87344cc2'
-app_id = '6468363d'
 
-url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key)
+def ingr_request():
+    ingredient = input("What ingredient do you want to search for? ")
+    app_key = 'a28413f876f1d5d0b52d989a87344cc2'
+    app_id = '6468363d'
 
-response = requests.get(url)
-recipe = response.json()
+    url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key)
 
-print(response)
-print(recipe)
+    response = requests.get(url)
+    recipe = response.json()
+    print(response)
+
+    # prints ALL the recipes related to the ingredient (including all the recipe details)
+    #print(recipe)
+
+    print("\n--------------\n")
+
+    # Prints the NAME of the recipe (there must be an easier way than this though?)
+    print(recipe["hits"][0]["recipe"]["label"])
+    print(recipe["hits"][1]["recipe"]["label"])
+
+ingr_request()
