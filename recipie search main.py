edab5430ded15dc26017773352b937f81869e0b7
Osaro <osaroabhus@gmail.com>
2022-08-22 18:11:54 +0100

    output all recipe names from the hits

--- recipie search main.py
+++ recipie search main.py
@@ -4,7 +4,17 @@ ingr = input("What ingredient do you want to search for? ")
 appkey = 'a28413f876f1d5d0b52d989a87344cc2'
 appID ='6468363d'
 
-url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingr, appkey, appID)
+url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingr, appID, appkey)
 
 response = requests.get(url)
 recipe = response.json()
+
+# find the number of "hits" aka the number of recipes found for the ingredients
+length = len(recipe["hits"])
+
+# run through all the recipes and print only their names
+for i in range(length):
+    print(recipe["hits"][i]["recipe"]["label"])
+
+def request():
+    pass
